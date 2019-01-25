17cbbd92c1676eedc4b4e38cee900de7a1729657
Richard Walker <digitalsadhu@gmail.com>
2019-01-25 17:55:53 +0100

    reverse delete of module

--- bundles/bundle-example-lib/attributes.js
+++ bundles/bundle-example-lib/attributes.js
@@ -1,21 +1,23 @@
 'use strict';
 
 module.exports = [
-  {
-    name: 'health',
-    base: 100,
-    formula: {
-      requires: [],
-      fn: function (character, health) {
-        return health + (character.level ** 2);
-      },
-    },
-  },
-  { name: 'energy', base: 100 },
-  { name: 'strength', base: 0 },
-  { name: 'agility', base: 0 },
-  { name: 'intellect', base: 0 },
-  { name: 'stamina', base: 0 },
-  { name: 'armor', base: 0 },
-  { name: 'critical', base: 0 },
+    { name: 'health', base: 0 },
+    { name: 'energy', base: 100 },
+    { name: 'strength', base: 0 },
+    { name: 'agility', base: 0 },
+    { name: 'intellect', base: 0 },
+    { name: 'stamina', base: 0 },
+    { name: 'armor', base: 0 },
+    { name: 'critical', base: 0 },
+
+    { name: 'proficiency', base: 0 },
+    { name: 'speed', base: 0 },
+    { name: 'lvl', base: 0 },
+    { name: 'ac', base: 0 },
+    { name: 'str', base: 0 },
+    { name: 'int', base: 0 },
+    { name: 'dex', base: 0 },
+    { name: 'con', base: 0 },
+    { name: 'cha', base: 0 },
+    { name: 'wis', base: 0 },
 ];
