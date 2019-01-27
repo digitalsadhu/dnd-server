834069d57c071c44ff8e5d9564ddc612fd899385
Richard Walker <digitalsadhu@gmail.com>
2019-01-27 23:58:24 +0100

    add help commands

--- bundles/ranvier-dice-roller/commands/roll.js
+++ bundles/ranvier-dice-roller/commands/roll.js
@@ -1,24 +1,22 @@
 'use strict';
 
 const { DiceRoller } = require('rpg-dice-roller');
+const { Broadcast } = require('ranvier');
 
-module.exports = srcPath => {
-    const Broadcast = require(srcPath + 'Broadcast');
-
-    return {
-        command: state => (args, player) => {
-            const roller = new DiceRoller();
-            Broadcast.sayAt(
-                player,
-                `You roll the dice! <yellow>${roller.roll(args)}</yellow>`
-            );
-            Broadcast.sayAtExcept(
-                player.room,
-                `<cyan>${
-                    player.name
-                }</cyan> rolls the dice! <yellow>${roller.roll(args)}</yellow>`,
-                [player]
-            );
-        },
-    };
+module.exports = {
+    usage: 'roll [dice roll]',
+    command: state => (args, player) => {
+        const roller = new DiceRoller();
+        Broadcast.sayAt(
+            player,
+            `You roll the dice! <yellow>${roller.roll(args)}</yellow>`
+        );
+        Broadcast.sayAtExcept(
+            player.room,
+            `<cyan>${player.name}</cyan> rolls the dice! <yellow>${roller.roll(
+                args
+            )}</yellow>`,
+            [player]
+        );
+    },
 };
